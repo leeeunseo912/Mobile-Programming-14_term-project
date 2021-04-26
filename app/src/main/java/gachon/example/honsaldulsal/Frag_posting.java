7ec0992c7f9a13446086c6c8ef2116662421f7b7
vbnm134678 <76089508+vbnm134678@users.noreply.github.com>
2021-04-26 20:57:39 +0900

    posting값 firebase 연동//미완성

--- app/src/main/java/gachon/example/honsaldulsal/Frag_posting.java
+++ app/src/main/java/gachon/example/honsaldulsal/Frag_posting.java
@@ -4,16 +4,63 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Frag_posting extends Fragment {
+
+    private DatabaseReference mDatabase;
+
+    public class User {
+        String[] info = new String[6];
+
+        public User() {
+        }
+
+        public User(String name, String quantity, String price, String location, String people, String etc) {
+            this.info[0] = name;
+            this.info[1] = quantity;
+            this.info[2] = price;
+            this.info[3] = location;
+            this.info[4] = people;
+            this.info[5] = etc;
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.activity_frag_posting,container,false);
+
+//        mDatabase = FirebaseDatabase.getInstance().getReference("info");
+//
+//
+//
+//        EditText Iname = v.findViewById(R.id.postItem);
+//        EditText Iquantity = v.findViewById(R.id.postQuantity);
+//        EditText Iprice = v.findViewById(R.id.postPrice);
+//        EditText Ilocation = v.findViewById(R.id.postLocation);
+//        EditText Ipeople = v.findViewById(R.id.postPeople);
+//        EditText IEtc = v.findViewById(R.id.postEtc);
+//
+//        Button postbtn = v.findViewById(R.id.postBtn);
+//
+//        postbtn.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view){
+//                User user = new User(Iname.getText().toString(), Iquantity.getText().toString(), Iprice.getText().toString(), Ilocation.getText().toString(), Ipeople.getText().toString(),
+//                        IEtc.getText().toString());
+//                mDatabase.child("info").child("product1").setValue(user);
+//            }
+//        });
+
         return v;
 
     }
